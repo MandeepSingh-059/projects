a1395cce01b1780256761c568e0168870b3462da
MandeepSingh-059 <1234mandeep1234@gmail.com>
2021-09-11 21:29:35 +0530

    React Alerts

--- TextUtils/my-app/src/App.js
+++ TextUtils/my-app/src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+import Alert from './components/Alert';
+// import About from './components/About';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState("light");//wether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const toggleMode = () => {
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      document.body.style.color = "white";
+      showAlert("Dark mode was enabled", "success");
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("Light mode was enabled", "success");
+    }
+  }
+  
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About"/>
-      <TextForm heading="Enter Text Below"/> 
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <TextForm heading="Enter Text Below" mode={mode} showAlert={showAlert}/> 
       {/*<About />*/}
     </>
   );
--- TextUtils/my-app/src/components/TextForm.js
+++ TextUtils/my-app/src/components/TextForm.js
@@ -4,12 +4,14 @@ export default function TextForm(props){
     
     const handleUpClick = () => {
         setText(text.toUpperCase());
+        props.showAlert("Converted to Uppercase", "success");
     }
     const handleOnChange = event => {
         setText(event.target.value);
     }
     const handleLoClick = () => {
         setText(text.toLowerCase());
+        props.showAlert("Converted to Lowercase", "success");
     }
     const handleMemeClick = () => {
         let str = '';
@@ -22,8 +24,22 @@ export default function TextForm(props){
             }
         }
         setText(str);
+        props.showAlert("Converted to MeMe CaSe", "success");
     }
     
+    const handleCpyText = () => {
+        var text = document.getElementById("textArea");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text Copied to ClipBoard", "success");
+    }
+
+    const selectedMode = {    
+        backgroundColor: props.mode === "dark" ? "#042743" : "white",
+        color: props.mode === "dark" ? "white" : "black"
+    }    
+
     const [text, setText] = useState('');
     const noOfWords = text.length === 0 ? text.split(" ").length-1 : text.split(" ").length;
 
@@ -33,10 +49,11 @@ export default function TextForm(props){
             <h4>
                 <label htmlFor="Textarea">{props.heading}</label>
             </h4>
-            <textarea className="form-control" id="Textarea" value={text} onChange={handleOnChange} rows="15"></textarea>
+            <textarea style={selectedMode} className="form-control" id="Textarea" value={text} onChange={handleOnChange} rows="15"></textarea>
             <button className="btn btn-primary mt-3 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mt-3 mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mt-3 mx-2" onClick={handleMemeClick}>Convert to MemeCase</button>
+            <button className="btn btn-primary mt-3 mx-2" onClick={handleCpyText}>Copy Text</button>
         </div>
         <div className="container my-3">
             <h4>Your Text Summary: </h4>
