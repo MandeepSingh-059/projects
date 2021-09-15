523384988b047a1c42adf3a018921197be0f744c
MandeepSingh-059 <1234mandeep1234@gmail.com>
2021-09-16 02:12:19 +0530

    NewsFill basic structure complete

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -6,15 +6,18 @@ import {
 
 export class NewsItem extends Component {
     render() {
+
+        let {title,description,author,imageUrl,newsUrl} = this.props;
+        
         return (
-            <div>
-                <Card max-width="500px">
-                    <CardImg top  src="/assets/318x180.svg" alt="Card image cap" />
+            <div className="my-3">
+                <Card style={{maxWidth : "100%"}}>
+                    <CardImg top src={imageUrl} alt="Card image cap" />
                     <CardBody>
-                        <CardTitle tag="h5">Card title</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                        <Button>Button</Button>
+                        <CardTitle tag="h5">{title}{`${title.length<70 ? "":"..."}`}</CardTitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">{`${author!=null ? "By-":""}`}{author}</CardSubtitle>
+                        <CardText>{description}...</CardText>
+                        <a className="btn btn-outline-success btn-sm" href={newsUrl} target="_blank">Read More</a>
                     </CardBody>
                 </Card>
             </div>
