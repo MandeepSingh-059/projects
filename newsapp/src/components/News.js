e16430d2aa6e0a01a7bdafdfe8d8231858a8d7ed
MandeepSingh-059 <123mandeep1234@gmail.com>
2022-01-14 18:05:10 +0530

    testing before new repo

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -20,14 +20,14 @@ const News = (props) => {
     const updateNews = async ()=> {
         props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`; 
-        setLoading(true)
+        setLoading(true);
         let data = await fetch(url);
         props.setProgress(30);
-        let parsedData = await data.json()
+        let parsedData = await data.json();
         props.setProgress(70);
-        setArticles(parsedData.articles)
-        setTotalResults(parsedData.totalResults)
-        setLoading(false)
+        setArticles(parsedData.articles);
+        setTotalResults(parsedData.totalResults);
+        setLoading(false);
         props.setProgress(100);
 
     }
