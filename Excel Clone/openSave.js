01cc63bd702d56cf6ad5ef1a76d73bb595d4deea
MandeepSingh-059 <123mandeep1234@gmail.com>
2022-02-28 01:19:28 +0530

    download json data implemented

--- Excel Clone/openSave.js
+++ Excel Clone/openSave.js
@@ -3,11 +3,28 @@ $(document).ready(function(){
     $(".menu-file").click(function (){
         $(".menu-file").append("<div class='btn-save'>Save</div>");
         $(".menu-file").append("<div class='btn-open'>Open</div>");
+        
+
+        
+        //download
+        $(".btn-save").click(function(){
+            let jsonData = JSON.stringify(cellData);
+            let file = new Blob([jsonData], {type: "application/json"});
+    
+            let a = document.createElement("a");
+            a.href = URL.createObjectURL(file);
+            a.download = "SheetData.json";
+            a.click();
+        })
+    
+
+
+
     });
+   
+   
     $(".menu-file").mouseleave(function (){
         $(".btn-save, .btn-open").remove();
     });
-
-
 
 });
